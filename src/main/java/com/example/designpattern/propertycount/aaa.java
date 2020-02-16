5696419d70ff1f8df4f1a19731d44081b555da13
zyy <731804840@qq.com>
2020-02-16 08:14:37 +0800

    设计模式学习项目

---
+++ src/main/java/com/example/designpattern/propertycount/aaa.java
@@ -0,0 +1,7 @@
+package com.example.designpattern.propertycount;
+
+/**
+ * Created by zyy on 20/2/15.
+ */
+public class aaa {
+}
---
+++ src/main/java/com/example/designpattern/propertycount/version1/EmailSender.java
@@ -0,0 +1,7 @@
+package com.example.designpattern.propertycount.version1;
+
+/**
+ * Created by zyy on 20/2/15.
+ */
+public class EmailSender {
+}
